b2517c173fa3429d840f158cb66039d2f5dff8b3
JunyiShangguan <2013301020076@whu.edu.cn>
2016-05-30 10:09:19 +0800

    figures

--- ex10_ch3.26/10.3poincare.py
+++ ex10_ch3.26/10.3poincare.py
@@ -14,33 +14,94 @@ def loren(x0,y0,z0,r,sigma,b,dt,T):
     return location#x,y,z,time
 
 
-def poincare(zero,non1,non2):
+def poincare(zero,non1,non2,k):
     poi=[[]for i in range(2)]
     for i in range(300000,len(zero)-1):
-        if zero[i]*zero[i+1]<=0:
+        if (zero[i]-k)*(zero[i+1]-k)<=0:
             poi[0].append(non1[i])
             poi[1].append(non2[i])
     return poi
 
-#Figure 3 poincare section
 sigma,b,dt,T=10,8./3,0.0001,200
 D=loren(1,0,0,25,sigma,b,dt,T)
-plt.figure(figsize=(14,6))
+
+#Figure 3.1 poincare section r=25
 plt.subplot(1,2,1)#x=0
-poix=poincare(D[0],D[1],D[2])
+poix=poincare(D[0],D[1],D[2],0)
 plt.scatter(poix[0],poix[1],s=10)
-plt.title('Fig.3.1 Lorentz Model, Poincare Section:z-y')
+plt.title('Fig.3.1 Lorentz Model, Poincare Section,r=25')
 plt.xlabel('y',fontsize=20)
 plt.ylabel('z',fontsize=20)
 plt.text(0,10.5,'phase space plot when x=0')
+
 plt.subplot(1,2,2)#y=0
-poiy=poincare(D[1],D[0],D[2])
+poiy=poincare(D[1],D[0],D[2],0)
 plt.scatter(poiy[0],poiy[1],s=10)
-plt.title('Fig.3.2 Lorentz Model, Poincare Section:z-x')
+plt.title('Fig.3.2 Lorentz Model, Poincare Section:r=25')
 plt.xlabel('x',fontsize=20)
 plt.ylabel('z',fontsize=20)
-plt.text(0,11,'phase space plot when x=0')
+plt.text(0,11,'phase space plot when y=0')
 plt.show()
+
+#Figure 3.3 poincare section r=25
+plt.subplot(2,2,1)#x=2
+poix=poincare(D[0],D[1],D[2],2)
+plt.scatter(poix[0],poix[1],s=10)
+plt.title('Fig.3.3 Poincare Section,r=25')
+plt.xlabel('y')
+plt.ylabel('z')
+plt.text(0,4,'Lorentz Model'+'\nphase space plot when x=2')
+
+plt.subplot(2,2,2)#x=-2
+poix=poincare(D[0],D[1],D[2],-2)
+plt.scatter(poix[0],poix[1],s=10)
+plt.title('Fig.3.4 Poincare Section,r=25')
+plt.xlabel('y')
+plt.ylabel('z')
+plt.text(0,4,'Lorentz Model'+'\nphase space plot when x=-2')
+
+plt.subplot(2,2,3)#y=2
+poiy=poincare(D[1],D[0],D[2],2)
+plt.scatter(poiy[0],poiy[1],s=10)
+plt.title('Fig.3.5 Poincare Section:r=25')
+plt.xlabel('x')
+plt.ylabel('z')
+plt.text(0,3,'Lorentz Model'+'\nphase space plot when y=2')
+
+plt.subplot(2,2,4)#y=-2
+poiy=poincare(D[1],D[0],D[2],-2)
+plt.scatter(poiy[0],poiy[1],s=10)
+plt.title('Fig.3.6 Poincare Section:r=25')
+plt.xlabel('x')
+plt.ylabel('z')
+plt.text(0,3,'Lorentz Model'+'\nphase space plot when y=-2')
+plt.show()
+
+
+D=loren(1,0,0,35,sigma,b,dt,T)
+#Figure3.3 poincare section,r=35
+plt.figure(figsize=(14,6))
+plt.subplot(1,2,1)#x=0
+poix=poincare(D[0],D[1],D[2],0)
+plt.scatter(poix[0],poix[1],s=10)
+plt.title('Fig.3.7 Lorentz Model, Poincare Section')
+plt.xlabel('y',fontsize=20)
+plt.ylabel('z',fontsize=20)
+plt.text(0,16,'r=35'+'\nphase space plot when x=0')
+
+plt.subplot(1,2,2)#y=0
+poiy=poincare(D[1],D[0],D[2],0)
+plt.scatter(poiy[0],poiy[1],s=10)
+plt.title('Fig.3.8 Lorentz Model, Poincare Section')
+plt.xlabel('x',fontsize=20)
+plt.ylabel('z',fontsize=20)
+plt.text(1,17,'r=35'+'\nphase space plot when y=0')
+plt.show()
+
+
+
+
+
 
 
 
